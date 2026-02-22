2b3bdfb0e50da795866eeafb8def19e22be66863
ReyAndrey21 <bitoleanu.eugen.f5a@student.ucv.ro>
2026-02-22 16:03:24 +0200

    Modified MovementSpeed and MouseSensitivity with new values

--- SPG-Proiect/Camera.h
+++ SPG-Proiect/Camera.h
@@ -25,7 +25,7 @@ public:
     float MouseSensitivity;
 
     Camera(glm::vec3 position = glm::vec3(0.0f, 0.0f, 3.0f), glm::vec3 up = glm::vec3(0.0f, 1.0f, 0.0f), float yaw = -90.0f, float pitch = 0.0f)
-        : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(50.0f), MouseSensitivity(0.1f) {
+        : Front(glm::vec3(0.0f, 0.0f, -1.0f)), MovementSpeed(70.0f), MouseSensitivity(0.3f) {
         Position = position;
         WorldUp = up;
         Yaw = yaw;
